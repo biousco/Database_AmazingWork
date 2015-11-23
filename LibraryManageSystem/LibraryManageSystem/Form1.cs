4cb207c49a138f5ed46d0dd58254fbc8ed811f06
Biousco <441936245@qq.com>
2015-11-23 20:50:01 +0800

    登录界面

--- LibraryManageSystem/LibraryManageSystem/Form1.cs
+++ LibraryManageSystem/LibraryManageSystem/Form1.cs
@@ -16,5 +16,10 @@ namespace LibraryManageSystem
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
