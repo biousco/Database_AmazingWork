99db208585183ae8f260fcdfb8867268ba1f7724
Biousco <441936245@qq.com>
2015-11-23 21:07:51 +0800

    搜索&借阅界面

--- LibraryManageSystem/LibraryManageSystem/Main.cs
+++ LibraryManageSystem/LibraryManageSystem/Main.cs
@@ -10,14 +10,29 @@ using System.Windows.Forms;
 
 namespace LibraryManageSystem
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
