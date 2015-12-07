a41221da664c0cafc70caab221e2c2493dcbc638
Biousco <441936245@qq.com>
2015-12-07 21:33:01 +0800

    修改目录

--- LibraryManageSystem/LMSTest/LoginTest.cs
+++ LibraryManageSystem/LMSTest/LoginTest.cs
@@ -26,4 +26,28 @@ namespace LMSTest
             Assert.AreEqual(expected, actual);
         }
     }
+
+    [TestClass()]
+    public class BookTests
+    {
+        [TestMethod()]
+        public void BorrowTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "00125415152";
+            viewer.Id = "s005";
+            manager.Id = "m003";
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book Program = new SQLDAL.Book();
+
+            actual = Program.Borrow(book, viewer, manager); //实际值
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
 }
--- LibraryManageSystem/SQLDALTests/BookTests.cs
+++ LibraryManageSystem/SQLDALTests/BookTests.cs
@@ -8,27 +8,5 @@ using System.Threading.Tasks;
 
 namespace SQLDAL.Tests
 {
-    [TestClass()]
-    public class BookTests
-    {
-        [TestMethod()]
-        public void BorrowTest()
-        {
-            Model.Book book = new Model.Book();
-            Model.Viewer viewer = new Model.Viewer();
-            Model.Manager manager = new Model.Manager();
-            book.Id = "00125415152";
-            viewer.Id = "s005";
-            manager.Id = "m003";
 
-            bool expected = true;    //期望值
-            bool actual;
-
-            SQLDAL.Book Program = new SQLDAL.Book();
-
-            actual = Program.Borrow(book, viewer, manager); //实际值
-            Assert.AreEqual(expected, actual);
-
-        }
-    }
 }
