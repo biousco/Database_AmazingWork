c56c68c81c7b67d329a5fca57e439836c7e40003
lynn0000 <610062426@qq.com>
2015-12-17 23:26:43 +0800

    补充了测试

--- LibraryManageSystem/LMSTest/Test.cs
+++ LibraryManageSystem/LMSTest/Test.cs
@@ -14,10 +14,32 @@ namespace LMSTest
     public class ViewerTests
     {
         [TestMethod]
+        public void VLoginPwdErrTest()
+        {
+            SQLDAL.Viewer target = new SQLDAL.Viewer();
+            string userId = "st";    
+            string userPassword = "1111";//密码错误
+            bool expected = false;    //期望值
+            bool actual;
+            actual = target.Login(userId, userPassword); //实际值
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void VLoginIdErrTest()
+        {
+            SQLDAL.Viewer target = new SQLDAL.Viewer();
+            string userId = "s000001";    //学号错误
+            string userPassword = "111111";
+            bool expected = false;    //期望值
+            bool actual;
+            actual = target.Login(userId, userPassword); //实际值
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
         public void VLoginTest()
         {
             SQLDAL.Viewer target = new SQLDAL.Viewer();
-            string userId = "st";    //用例
+            string userId = "s001";
             string userPassword = "111111";
             bool expected = true;    //期望值
             bool actual;
@@ -43,9 +65,31 @@ namespace LMSTest
         public void MLoginTest()
         {
             SQLDAL.Manager target = new SQLDAL.Manager();
-            string userId = "manager";    //用例
+            string userId = "manager";    
             string userPassword = "123456";
             bool expected = true;    //期望值
+            bool actual;
+            actual = target.Login(userId, userPassword); //实际值
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void MLoginIdErrTest()
+        {
+            SQLDAL.Manager target = new SQLDAL.Manager();
+            string userId = "m";    //账号错误
+            string userPassword = "123456";
+            bool expected = false;    //期望值
+            bool actual;
+            actual = target.Login(userId, userPassword); //实际值
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void MLoginPwdErrTest()
+        {
+            SQLDAL.Manager target = new SQLDAL.Manager();
+            string userId = "manager";
+            string userPassword = "123";
+            bool expected = false;    //期望值
             bool actual;
             actual = target.Login(userId, userPassword); //实际值
             Assert.AreEqual(expected, actual);
@@ -54,7 +98,7 @@ namespace LMSTest
         public void GetManagerNameTest()
         {
             SQLDAL.Manager target = new SQLDAL.Manager();
-            string r_id = "manager";    //用例
+            string r_id = "manager";    
             string expected = "Test";    //期望值
             string actual;
             actual = target.GetManagerName(r_id).Name.ToString(); //实际值
@@ -75,13 +119,68 @@ namespace LMSTest
             viewer.Id = "st";
             manager.Id = "manager";
 
-            bool expected = true;    //期望值
+            bool expected = false;    //期望值//因为数据库中已经存在这一项数据 所以为false
             bool actual;
 
             SQLDAL.Book target = new SQLDAL.Book();
             actual = target.Borrow(book, viewer, manager); //实际值
             Assert.AreEqual(expected, actual);
            
+        }
+        [TestMethod()]
+        public void BorrowVIdErrTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "book";
+            viewer.Id = "s000001";//学号错误
+            manager.Id = "manager";
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book target = new SQLDAL.Book();
+            actual = target.Borrow(book, viewer, manager); //实际值
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void BorrowMIdErrTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "book";
+            viewer.Id = "s000001";
+            manager.Id = "m";//帐号错误
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book target = new SQLDAL.Book();
+            actual = target.Borrow(book, viewer, manager); //实际值
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void MulBorrowTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "9787513318525";
+            viewer.Id = "st";
+            manager.Id = "manager";
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book target = new SQLDAL.Book();
+            target.Borrow(book, viewer, manager);//第一次借
+            actual = target.Borrow(book, viewer, manager); //实际值//第二次借
+            Assert.AreEqual(expected, actual);
+            //由于系统限制每个人只能借同一本书一次 所以测试时只有第一次是能成功
         }
         [TestMethod()]
         public void ReturnTest()
@@ -93,13 +192,84 @@ namespace LMSTest
             viewer.Id = "st";
             manager.Id = "manager";
 
-            bool expected = true;    //期望值
+            bool expected = false;    //期望值//因为数据库中已经存在这一项数据 所以为false
             bool actual;
 
             SQLDAL.Book target = new SQLDAL.Book();
             actual = target.ReturnBook(viewer, book, manager); //实际值
             Assert.AreEqual(expected, actual);
 
+        }
+        [TestMethod()]
+        public void MulReturnTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "9787513318525";
+            viewer.Id = "st";
+            manager.Id = "manager";
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book target = new SQLDAL.Book();
+            target.ReturnBook(viewer, book, manager); //第一次还
+            actual = target.ReturnBook(viewer, book, manager); //实际值//第二次还
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void ReturnVIdErrTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "book";
+            viewer.Id = "s00001";//学号错误
+            manager.Id = "manager";
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book target = new SQLDAL.Book();
+            actual = target.ReturnBook(viewer, book, manager); //实际值
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void ReturnBIdErrTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "boo";//书号错误
+            viewer.Id = "st";
+            manager.Id = "manager";
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book target = new SQLDAL.Book();
+            actual = target.ReturnBook(viewer, book, manager); //实际值
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ReturnMIdErrTest()
+        {
+            Model.Book book = new Model.Book();
+            Model.Viewer viewer = new Model.Viewer();
+            Model.Manager manager = new Model.Manager();
+            book.Id = "book";
+            viewer.Id = "st";
+            manager.Id = "m";//账号错误
+
+            bool expected = false;    //期望值
+            bool actual;
+
+            SQLDAL.Book target = new SQLDAL.Book();
+            actual = target.ReturnBook(viewer, book, manager); //实际值
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod()]
         public void GetListTest()
